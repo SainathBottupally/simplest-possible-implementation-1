9742da7f244b4b6ba96dfebd2e9a3690c3df9dcc
Sainath Bottupally <sai@Sainaths-MacBook-Pro.local>
2021-01-03 23:24:51 -0800

    Added implementaion of test

---
+++ src/main/java/simplest/possible/implementation/PrimeFactors.java
@@ -0,0 +1,13 @@
+package simplest.possible.implementation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrimeFactors {
+    public <T> List<T> of(int number) {
+        if(number == 1){
+            return (List<T>) new ArrayList<Integer>();
+        }
+        return (List<T>) new ArrayList<Integer>();
+    }
+}
--- src/test/java/simplest/possible/implementation/PrimeFactorsTest.java
+++ src/test/java/simplest/possible/implementation/PrimeFactorsTest.java
@@ -3,11 +3,14 @@ package simplest.possible.implementation;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static java.util.Collections.emptyList;
 
 public class PrimeFactorsTest {
     @Test
     void thereAreNoPrimeFactorsFor1() {
-        Assertions.assertEquals(emptyList(), new PrimeFactors().of(1));
+        List<Integer> factors = new PrimeFactors().of(1);
+        Assertions.assertEquals(emptyList(), factors);
     }
 }
